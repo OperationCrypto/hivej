491252e1da17e307e22a9011a6145b6002077caf
marvin-we <marvin.weck@web.de>
2020-08-30 19:55:34 +0200

    Align and add missing headers

--- core/src/main/java/org/operationcrypto/hivej/enums/RequestMethod.java
+++ core/src/main/java/org/operationcrypto/hivej/enums/RequestMethod.java
@@ -1,18 +1,18 @@
 /*
- *     This file is part of SteemJ (formerly known as 'Steem-Java-Api-Wrapper')
+ *     This file is part of HiveJ
  * 
- *     SteemJ is free software: you can redistribute it and/or modify
+ *     HiveJ is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  * 
- *     SteemJ is distributed in the hope that it will be useful,
+ *     HiveJ is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  * 
  *     You should have received a copy of the GNU General Public License
- *     along with SteemJ.  If not, see <http://www.gnu.org/licenses/>.
+ *     along with HiveJ.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.operationcrypto.hivej.enums;
 
