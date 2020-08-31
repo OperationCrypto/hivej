b7404cf71889324fbfc04a7a500204a60182da7f
Johannes <johannes.doeing@gmail.com>
2020-08-31 18:07:25 +0200

    increased junit coverage and added java doc

--- core/src/test/java/communication/DatabaseAPITest.java
+++ core/src/test/java/communication/DatabaseAPITest.java
@@ -22,13 +22,20 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.operationcrypto.hivej.api.database.model.FindVotesArgs;
 import org.operationcrypto.hivej.api.database.model.FindVotesReturn;
+import org.operationcrypto.hivej.api.database.model.Fund;
 import org.operationcrypto.hivej.api.database.model.GetRewardFundsReturn;
+import org.operationcrypto.hivej.api.database.model.RewardBalance;
 import org.operationcrypto.hivej.communication.CommunicationHandler;
 import org.operationcrypto.hivej.enums.HiveApiType;
 import org.operationcrypto.hivej.enums.RequestMethod;
 import org.operationcrypto.hivej.jrpc.JsonRPCRequest;
 
 public class DatabaseAPITest {
+	/**
+	 * Tests the database api find votes request and response by passing a user and permlink.
+	 * 
+	 * @throws Throwable
+	 */
 	@Test
 	public void testFindVotes() throws Throwable {
 		String permlink = "steemj-dev-diary-9-14-01-2018";
@@ -44,6 +51,11 @@ public class DatabaseAPITest {
 		assertEquals(author, result.getVotes().get(0).getAuthor());
 	}
 	
+	/**
+	 * Tests the database api get reward funds request (no parameters) and response.
+	 * 
+	 * @throws Throwable
+	 */	
 	@Test
 	public void testGetRewardFunds() throws Throwable {
 		JsonRPCRequest request = new JsonRPCRequest(HiveApiType.DATABASE_API, RequestMethod.GET_REWARD_FUNDS,
@@ -52,8 +64,20 @@ public class DatabaseAPITest {
 		CommunicationHandler communicationHandler = new CommunicationHandler();
 		GetRewardFundsReturn result = communicationHandler.performRequest(request, GetRewardFundsReturn.class).get(0);
 
-		assertNotNull(result);
-		assertNotNull(result.getFunds().get(0));
+		Fund fund = result.getFunds().get(0);
+		assertNotNull(fund.getId());
+		assertNotNull(fund.getName());
+		RewardBalance rewardBalance = fund.getRewardBalance();
+		assertNotNull(rewardBalance.getAmount());
+		assertNotNull(rewardBalance.getNai());
+		assertNotNull(rewardBalance.getPrecision());
+		assertNotNull(fund.getAuthorRewardCurve());
+		assertNotNull(fund.getPercentContentRewards());
+		assertNotNull(fund.getPercentCurationRewards());
+		assertNotNull(fund.getRecentClaims());
+		assertNotNull(fund.getLastUpdate());
+		assertNotNull(fund.getContentConstant());
+		assertNotNull(fund.getCurationRewardCurve());
 	}
 }
  
