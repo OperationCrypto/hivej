f52ca8bf9d477be50eae7969d4f67ac4581ed00e
marvin-we <marvin.weck@web.de>
2020-09-02 20:37:31 +0200

    Fix boolean serialization

---
+++ core/src/main/java/org/operationcrypto/hivej/base/serializer/BooleanSerializer.java
@@ -0,0 +1,39 @@
+/*
+ *     This file is part of SteemJ (formerly known as 'Steem-Java-Api-Wrapper')
+ * 
+ *     SteemJ is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     SteemJ is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with SteemJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.operationcrypto.hivej.base.serializer;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+/**
+ * By default, Jackson does not serialize bool variables as strings - As the
+ * Hive node requires the string representation, this custom serializer had to
+ * be created.
+ * 
+ * @author <a href="https://github.com/marvin-we">marvin-we</a>
+ */
+public class BooleanSerializer extends JsonSerializer<Boolean> {
+
+    @Override
+    public void serialize(Boolean boolValue, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
+        jsonGenerator.writeString(boolValue.toString());
+    }
+}
